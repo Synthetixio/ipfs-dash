fe6274c438f45d7c8c5cab351e6e5b28da9bc05e
Noisekit <nikita@cc.snxdao.io>
2023-06-09 12:51:34 +0800

    IPFS State update

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+async function updateState() {
+  const res = await fetch('/api');
+  if (!res.ok) {
+    const text = await res.text();
+    console.error(new Error(text));
+    return;
+  }
+  window.postMessage({ type: 'state', payload: await res.json() });
+}
+
+function render() {
+  document.querySelector('#state').innerText = JSON.stringify(window.state, null, 2);
+}
+
 function init() {
   console.log('DOM loaded');
+
+  window.addEventListener('message', (e) => {
+    if (e.data.type === 'state') {
+      window.state = e.data.payload;
+      render();
+    }
+  });
+
+  render();
+
+  setInterval(updateState, 5_000);
 }
 
 document.addEventListener('DOMContentLoaded', init);
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const cp = require('child_process');
 
-const mimeTypes = {
-  '.js': 'text/javascript',
-  '.css': 'text/css',
-  '.json': 'application/json',
-  '.png': 'image/png',
-  '.svg': 'image/svg+xml',
-};
-
+const indexHtml = fs.readFileSync('./index.html', 'utf-8');
 const files = {
-  '/index.html': {
-    content: fs.readFileSync('./index.html', 'utf-8'),
+  '/': {
+    content: indexHtml,
     contentType: 'text/html',
+  },
+  '/favicon.ico': {
+    content: fs.readFileSync('./favicon.ico'),
+    contentType: 'image/x-icon',
   },
   '/main.js': {
     content: fs.readFileSync('./main.js', 'utf-8'),
@@ -24,9 +22,21 @@ const files = {
     contentType: 'text/css',
   },
 };
-files['/'] = files['/index.html'];
+
+const state = {
+  peers: [],
+};
+
+function render() {
+  return (files['/'].content = indexHtml.replace('%%__STATE__%%', JSON.stringify(state, null, 2)));
+}
 
 const server = http.createServer((req, res) => {
+  if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(state, null, 2), 'utf-8');
+    return;
+  }
   if (req.url in files) {
     const file = files[req.url];
     res.writeHead(200, { 'Content-Type': file.contentType });
@@ -38,4 +48,31 @@ const server = http.createServer((req, res) => {
   return;
 });
 
-server.listen(3000, '0.0.0.0', () => console.log('Server running at http://0.0.0.0:3000/'));
+async function updatePeers() {
+  const peers = await new Promise((resolve) =>
+    cp.exec("ipfs-cluster-ctl --enc=json peers ls | jq '[inputs]'", (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return resolve([]);
+      }
+      if (stderr) {
+        console.error(new Error(stderr));
+        return resolve([]);
+      }
+      try {
+        const result = JSON.parse(stdout);
+        return resolve(result.map(({ id, peername, version }) => ({ id, peername, version })));
+      } catch (e) {
+        return resolve([]);
+      }
+      return resolve([]);
+    })
+  );
+  console.log(`peers`, peers);
+  Object.assign(state, { peers });
+  render();
+}
+setInterval(updatePeers, 10_000);
+Promise.all([updatePeers()]).then(() =>
+  server.listen(3000, '0.0.0.0', () => console.log('Server running at http://0.0.0.0:3000/'))
+);
